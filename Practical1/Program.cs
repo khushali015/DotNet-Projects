52ca479bb46350ea7e5325dca704f8dea11f1661
khushali015 <shahkd1999@gmail.com>
2020-01-21 15:37:41 +0530

    Added Pattern

--- Practical1/Program.cs
+++ Practical1/Program.cs
@@ -8,8 +8,71 @@ namespace Practical1
 {
     class Program
     {
+        static void Pattern1()
+        {
+            Console.WriteLine("Pattern 1");
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine();
+                for (int j = 1; j <= 5; j++)
+                {
+                    Console.Write("*");
+                }
+            }
+        }
+        static void Pattern2()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Pattern 2");
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine();
+                for (int j = 1; j <= i; j++)
+                    Console.Write("*");
+            }
+        }
+        static void Pattern3()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Pattern 3");
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine();
+                for (int j = 5; j > i; j--)
+                    Console.Write(" ");
+                for (int k = 1; k <= i; k++)
+                    Console.Write("*");
+            }
+        }
+        static void Pattern4()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Pattern 4");
+            for (int i=1;i<=4;i++)
+            {
+                Console.WriteLine();
+                for(int j=4;j>i;j--)
+                    Console.Write(" ");
+                for(int k=1;k<=i;k++)
+                    Console.Write("* ");
+            }
+            for(int i=3;i>=1;i--)
+            {
+                Console.WriteLine();
+                for (int j = 3; j > i; j--)
+                    Console.Write(" ");
+                for (int k = 1; k <= i; k++)
+                    Console.Write(" *");
+            }
+        }
         static void Main(string[] args)
         {
+            Pattern1();
+            Pattern2();
+            Pattern3();
+            Pattern4();
+            
+            Console.Read();
         }
     }
 }
